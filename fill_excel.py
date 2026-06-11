a12402025f2eed3a1539b74a062506d5177591ba
Proname11 <projectrr11@gmail.com>
2026-06-11 16:05:36 +0700

    Update fill_excel.py

--- fill_excel.py
+++ fill_excel.py
@@ -2,13 +2,13 @@ import gspread
 import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
 
-
-def get_google_sheet_data(spreadsheet_id):
+def get_client():
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds_dict = st.secrets["gcp_service_account"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-    client = gspread.authorize(creds)
-    
-    # ต้องใช้ open_by_key เท่านั้น!
+    return gspread.authorize(creds)
+
+def save_to_sheet(spreadsheet_id, data_list):
+    client = get_client()
     sheet = client.open_by_key(spreadsheet_id).sheet1
-    return sheet.get_all_values()
+    sheet.append_row(data_list)
