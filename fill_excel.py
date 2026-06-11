22344311ca385d580e24db21adad1f5830794b52
Proname11 <projectrr11@gmail.com>
2026-06-11 16:03:56 +0700

    Update fill_excel.py

--- fill_excel.py
+++ fill_excel.py
@@ -2,15 +2,13 @@ import gspread
 import streamlit as st
 from oauth2client.service_account import ServiceAccountCredentials
 
-def get_client():
-    # กำหนดสิทธิ์
+
+def get_google_sheet_data(spreadsheet_id):
     scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
     creds_dict = st.secrets["gcp_service_account"]
     creds = ServiceAccountCredentials.from_json_keyfile_dict(creds_dict, scope)
-    return gspread.authorize(creds)
-
-def get_sheet_data(spreadsheet_id):
-    # เชื่อมต่อและดึงข้อมูล
-    client = get_client()
+    client = gspread.authorize(creds)
+    
+    # ต้องใช้ open_by_key เท่านั้น!
     sheet = client.open_by_key(spreadsheet_id).sheet1
     return sheet.get_all_values()
